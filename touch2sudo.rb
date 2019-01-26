1537e8cf4f25321fdf63d8ac7dd80c1207a49a4c
Security Sauce <prbinu@users.noreply.github.com>
2019-01-26 13:44:33 -0800

    Create touch2sudo.rb

---
+++ touch2sudo.rb
@@ -0,0 +1,17 @@
+class touch2sudo < Formula
+  desc "A standalone program for two-factor authentication using Mac Touch ID"
+  homepage "https://github.com/prbinu/touch2sudo"
+  url "https://github.com/prbinu/touch2sudo/releases/download/v0.1/touch2sudo-0.1.tgz"
+  sha256 "a32ecf3ec34ee6b8b3ebc678c32ebdba56e5d94e47aa7381cf8ab90a60fb256c"
+  version "0.1"
+ 
+  bottle :unneeded
+
+  def install
+    bin.install "touch2sudo"
+  end
+  
+  test do
+    system "#{bin}/touch2sudo", "-h"
+  end
+end
