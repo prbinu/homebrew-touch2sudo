1a97f4577285da218498612869c2d9dfe9a5d24d
Security Sauce <prbinu@users.noreply.github.com>
2022-01-19 12:36:14 -0800

    Deprecated bottle :unneeded #1

--- touch2sudo.rb
+++ touch2sudo.rb
@@ -4,8 +4,6 @@ class Touch2sudo < Formula
   url "https://github.com/prbinu/touch2sudo/releases/download/v0.1/touch2sudo-0.1.tgz"
   sha256 "a32ecf3ec34ee6b8b3ebc678c32ebdba56e5d94e47aa7381cf8ab90a60fb256c"
   version "0.1"
- 
-  bottle :unneeded
 
   def install
     bin.install "touch2sudo"
